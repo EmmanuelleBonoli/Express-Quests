405258a635e00bf79e9fe31929df34243f481606
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-11-28 16:30:30 +0100

    ajout delete user & movie

--- src/app.js
+++ src/app.js
@@ -19,6 +19,9 @@ app.put("/api/users/:id", validateUser, userControllers.updateUser);
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
 app.post("/api/users", validateUser, userControllers.postUser);
 
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+app.delete("/api/users/:id", userControllers.deleteUser);
+
 module.exports = app;
 
 
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -113,14 +113,30 @@ const postMovie = (req, res) => {
       res.sendStatus(422);
     });
   }
- 
-  
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
-  updateMovie
+  updateMovie,
+  deleteMovie
 };
